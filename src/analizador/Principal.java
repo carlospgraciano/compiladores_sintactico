7ee601343188055ac09257ffc5ea3a2a7079f071
Carlos Polanco <carlosfollows@gmail.com>
2020-07-24 21:44:31 -0400

    Setup the generation of JFlex and Java_Cup

--- src/analizador/Principal.java
+++ src/analizador/Principal.java
@@ -1,17 +1,53 @@
 package analizador;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java_cup.internal_error;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws internal_error, IOException, Exception {
 		// TODO Auto-generated method stub
-		String ruta = new String("C:/Users/carlo/Documents/Teoria_Compiladores/compiladores_sintactico/src/analizador/Lexer.flex");
-		generarLexer(ruta);
+		String ruta1 = new String("C:/Users/carlo/Documents/Teoria_Compiladores/compiladores_sintactico/src/analizador/Lexer.flex");
+		String ruta2 = new String("C:/Users/carlo/Documents/Teoria_Compiladores/compiladores_sintactico/src/analizador/LexerCup.flex");
+		String[] rutaS = {"-parser", "Sintax", "C:/Users/carlo/Documents/Teoria_Compiladores/compiladores_sintactico/src/analizador/Sintax.cup"};
+		generar(ruta1, ruta2, rutaS);
 	}
 	
-	public static void generarLexer(String ruta) {
-		File archivo = new File(ruta);
+	public static void generar(String ruta1, String ruta2, String[] rutaS) throws internal_error, IOException, Exception {
+		File archivo;
+		archivo = new File(ruta1);
 		JFlex.Main.generate(archivo);
+		archivo = new File(ruta2);
+		JFlex.Main.generate(archivo);
+		java_cup.Main.main(rutaS);
+		
+		/* Ruta a la tabla de simbolos generada por CUP */
+		Path rutaSym = Paths.get("C:/Users/carlo/Documents/Teoria_Compiladores/compiladores_sintactico/src/analizador/sym.java");
+		/* Borra el archivo si ya existe en el proyecto */
+		if (Files.exists(rutaSym)) {
+			Files.delete(rutaSym);
+		}
+		/* Mueve el archivo de simbolos generado por CUP al directorio del proyecto */
+		Files.move(
+				Paths.get("C:/Users/carlo/Documents/Teoria_Compiladores/compiladores_sintactico/sym.java"),
+				rutaSym
+		);
+		
+		/* Ruta al archivo de sintactica generada por CUP */
+		Path rutaSintax = Paths.get("C:/Users/carlo/Documents/Teoria_Compiladores/compiladores_sintactico/src/analizador/Sintax.java");
+		/* Borra el archivo si ya existe en el proyecto */
+		if (Files.exists(rutaSintax)) {
+			Files.delete(rutaSintax);
+		}
+		/* Mueve el archivo de sintactica generado por CUP al directorio del proyecto */
+		Files.move(
+				Paths.get("C:/Users/carlo/Documents/Teoria_Compiladores/compiladores_sintactico/Sintax.java"),
+				rutaSintax
+		);
 	}
 }
